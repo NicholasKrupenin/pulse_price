223feacad416661289e8635eeb883532a5ae0aa7
Nicholas Krupenin <krupenin.nikolay@gmail.com>
2023-09-25 16:43:06 +0400

    add render spec

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe PagesHelper, type: :helper do
       expect(normalizing_content).to eq '<b>A</b> <i>B</i> <a href=A/a>B</a>'
     end
   end
+
+  describe "when pass root page" do
+    let(:sample_page) { create(:page, name: 'A') }
+
+    specify "#render_tree" do
+      complete_render = helper.render_tree([sample_page])
+      expect(complete_render).to eq "<br><a href=\"A\">A</a>"
+    end
+  end
 end
 
